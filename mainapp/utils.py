83dd0e35e7e5668d81bb97ac84aeb1d8b390c81c
Pavlenkovv <pavlenko.vyacheslav@gmail.com>
2021-02-22 14:00:25 +0200

    for debian

--- mainapp/utils.py
+++ mainapp/utils.py
@@ -3,7 +3,6 @@ from django.db import models
 
 def recalc_cart(cart):
     cart_data = cart.products.aggregate(models.Sum('final_price'), models.Count('id'))
-    print(cart_data)
     if cart_data.get('final_price__sum'):
         cart.final_price = cart_data['final_price__sum']
     else:
