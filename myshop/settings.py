b940482ade8c0d33d391a0b054f9c4dad52e435f
Pavlenkovv <pavlenko.vyacheslav@gmail.com>
2021-02-22 16:52:54 +0200

    for debian ALLOWED_HOSTS update

--- myshop/settings.py
+++ myshop/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 if DEBUG:
     STATIC_DIR = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_DIRS = [
         STATIC_DIR,
         '/home/pavlenko/smartshop/smartshop_deb/static/',
